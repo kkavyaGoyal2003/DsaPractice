4c90ee1762b9d64db42eecbd1656b1903ed34bf7
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-04-29 11:07:35 +0530

    without coversion

--- src/CheckSetBit.java
+++ src/CheckSetBit.java
@@ -1,26 +1,20 @@
+import java.util.Scanner;
+
 public class CheckSetBit {
     public static void main(String[] args) {
-        int n = 13;
-        int i = 1;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number and the ith bit you want to check:");
+        int n = scan.nextInt();
+        int i = scan.nextInt();
 
-        String str = covertToBinary1(n);
-        int len = str.length();
-
-        if(str.charAt(len - i -1) == '1') System.out.println("it has a set bit");
-        else System.out.println("not a set bit");
-    }
-
-    public static String covertToBinary1(int n) {
-        StringBuilder str = new StringBuilder();
-
-        while(n > 2) {
-            int rem = n % 2;
-            str.append(rem);
-            n /= 2;
+        int temp = 1 << i;
+        int  ans = n & temp;
+        if(ans > 0) {
+            System.out.println("It is Set");
+        } else {
+            System.out.println("It is Unset");
         }
 
-        str.append(n);
-        return str.reverse().toString();
-    }
 
+    }
 }
